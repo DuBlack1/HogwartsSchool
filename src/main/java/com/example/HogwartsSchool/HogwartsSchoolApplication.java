d21a29a2a22a583a8acd18746141b4116d85ab50
DuBlack1 <asya.schorikowa@yandex.ru>
2023-10-08 20:41:59 +0500

    Initial commit

---
+++ src/main/java/com/example/HogwartsSchool/HogwartsSchoolApplication.java
@@ -0,0 +1,16 @@
+package com.example.HogwartsSchool;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class HogwartsSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HogwartsSchool/HogwartsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HogwartsSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
